b8d96493dc5ee9f900ef1f7c25d4a7d9de328c45
drozd <mayakovsky03@gmail.com>
2025-12-12 07:16:42 +0200

    added new function deposit calculation

--- index.js
+++ index.js
@@ -10,17 +10,40 @@
 // const weather = (temp && willRain)
 // console.log(weather);
 
-function bark(name, weight) {
-    if (weight > 10) {
-        console.log(name + ' woof!')
-    } else {
-        console.log(name + ' meow!')
+// function bark(name, weight) {
+//     if (weight > 10) {
+//         console.log(name + ' woof!')
+//     } else {
+//         console.log(name + ' meow!')
+//     }
+// }
+//
+// bark('cat', 5);
+// bark('dog', 11);
+// bark('cat', 3);
+// bark('dog', 23);
+
+
+const depositAmount = 1000;
+const depositPeriodRate = 3;
+const depositPeriod = 3;
+
+function getDepositIncome(amount, rate, period) {
+
+    let depositIncome = 0;
+    let count = 0
+
+    while (count < period) {
+        depositIncome = depositIncome + amount * (rate / 100);
+        count++;
     }
+    return depositIncome;
 }
 
-bark('cat', 5);
-bark('dog', 11);
-bark('cat', 3);
-bark('dog', 23);
+const depositIncome1 = getDepositIncome(depositAmount, depositPeriodRate, depositPeriod)
+const depositIncome2 = getDepositIncome(3000, 4, 5)
 
+const totalIncome = depositIncome1 + depositIncome2
+
+console.log(totalIncome);
 
